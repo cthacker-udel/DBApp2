6129c71a67ee1abb21067b7b14442d2afa5435ad
Cameron Thacker <cthacker@udel.edu>
2021-12-23 17:39:26 -0500

    Succesffully added data transfer authentication via validation pipes

--- server/src/dbtest/dbuser/custompipes/userdto.to.userentity.pipe.ts
+++ server/src/dbtest/dbuser/custompipes/userdto.to.userentity.pipe.ts
@@ -1,14 +1,24 @@
 import { DBUserEntity } from './../entities/dbuser.entity';
 import { DBUserDto } from './../dto/dbuser.dto';
 
-import { ArgumentMetadata, PipeTransform } from "@nestjs/common";
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { validateOrReject } from 'class-validator';
+import { plainToClass } from 'class-transformer';
 
 
 
 export class UserDTOToUserEntityPipe implements PipeTransform {
 
-    transform(value: DBUserDto, metadata: ArgumentMetadata) {
-       return new DBUserEntity(value.username, value.password);
+    async transform(value: DBUserDto, metadata: ArgumentMetadata) {
+        const convertedJSONToDto = plainToClass(DBUserDto, value);
+        try{
+            await validateOrReject(convertedJSONToDto).catch(errors => {
+                throw new BadRequestException(errors)
+            });
+            return plainToClass(DBUserEntity, value);
+        } catch (errors) {
+            throw new BadRequestException('Invalid user posted');
+        }
     }
 
 }
--- server/src/dbtest/dbuser/dto/dbuser.dto.ts
+++ server/src/dbtest/dbuser/dto/dbuser.dto.ts
@@ -1,10 +1,16 @@
-import { IsString } from "class-validator";
+import { IsInt, IsString, Max, Min } from "class-validator";
 
 export class DBUserDto {
+
+    constructor(user: string, pass: string) {
+        this.username = user;
+        this.password = pass;
+    }
 
     @IsString()
     username: string
 
     @IsString()
     password: string
+
 };
