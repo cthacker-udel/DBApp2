80d6656af8761f7b073796f65ae9e00ae220e79d
Cameron Thacker <cthacker@udel.edu>
2021-12-29 04:28:58 -0500

    Added base classes to project

---
+++ server/src/modules/data/data.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from "@nestjs/common";
+
+
+
+@Controller('/api/public')
+export class DataController {
+
+
+
+};
---
+++ server/src/modules/data/data.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+
+
+
+@Module({
+
+    imports: [],
+    exports: [],
+    controllers: [],
+    providers: []
+
+}) export class DataModule{};
