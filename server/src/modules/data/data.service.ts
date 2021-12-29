5dd3746bc51f5eb2ebee3d8860a139f93215b255
Cameron Thacker <cthacker@udel.edu>
2021-12-29 06:44:38 -0500

    Added addTicket functionality

--- server/src/modules/data/data.service.ts
+++ server/src/modules/data/data.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { AddTicketRequestDTO } from "src/shared/api/private/post/AddTicketRequest.dto";
+import { AddTicketRequestEntity } from "src/shared/entities/mongodb/private/AddTicketRequestEntity.entity";
+import { EntityTarget, getConnection, getMongoManager } from "typeorm";
 
 @Injectable()
 export class DataService {
 
-    
+    private getMongoRepo<T>(repo: EntityTarget<T>) { return getConnection('mongo').getRepository<T>(repo)}
+
+    async addTicket(addTicket: AddTicketRequestEntity) {
+        const mongoRepo = this.getMongoRepo<AddTicketRequestEntity>(AddTicketRequestEntity);
+        try{
+            mongoRepo.insert(addTicket);
+        } catch (err) {
+            throw new BadRequestException('Invalid Post Request');
+        }
+    }
 
 };
