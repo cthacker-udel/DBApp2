fff71c1e2dd45a162729b704bc17f91c28807397
Cameron Thacker <cthacker@udel.edu>
2021-12-29 06:59:08 -0500

    Added basic getters to data.controller.ts

--- server/src/modules/data/data.controller.ts
+++ server/src/modules/data/data.controller.ts
@@ -1,10 +1,28 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 
 
 
-@Controller('/api/public')
+@Controller('/api')
 export class DataController {
 
+    @Get('/public/number_of_tickets/total')
+    async getNumberOfTickets() {
+        // finds number of tickets, can be public endpoint
+    };
 
+    @Get('/public/number_of_tickets/date/:date')
+    async getNumberOfTicketsDate(@Param('date') date: string) {
+        // finds number of tickets during specified date, can be public
+    };
+
+    @Get('/public/number_of_tickets/status/:status')
+    async getNumberOfTicketsStatus(@Param('status') status: string) {
+        // finds number of tickets with specified status
+    };
+
+    @Get('/public/number_of_tickets/priority/:number')
+    async getNumberOfTicketsPriority(@Param('number') number: string) {
+        // passes number as string, gather number of tickets with set priority
+    };
 
 };
