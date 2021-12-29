fddb3032b81001ae8aaa08bc27dc9538bd2dc98f
Cameron Thacker <cthacker@udel.edu>
2021-12-29 07:08:31 -0500

    Added FindTicketPipeline that converts the dto argument sent over the request to an entity to search for in the database

--- server/src/modules/data/pipeline/findticket.pipe.ts
+++ server/src/modules/data/pipeline/findticket.pipe.ts
@@ -1,12 +1,21 @@
+import { BadRequestException } from '@nestjs/common';
+import { FindTicketRequestDTO } from './../../../shared/api/private/get/FindTicketRequest.dto';
 import { ArgumentMetadata, ExecutionContext, Injectable, PipeTransform } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { FindTicketRequestEntity } from 'src/shared/entities/mongodb/private/get/FindTicketRequest.entity';
+import { validateOrReject } from 'class-validator';
 
 @Injectable()
 export class FindTicketPipe implements PipeTransform{
 
-    async transform(value: FindTicket, metadata: ArgumentMetadata) {
-
-        
-
+    async transform(value: FindTicketRequestDTO, metadata: ArgumentMetadata) {
+        const ConvertedToEntity = plainToClass(FindTicketRequestEntity, value);
+        try {
+            await validateOrReject(ConvertedToEntity);
+            return ConvertedToEntity;
+        } catch (errors) {
+            throw new BadRequestException("Invalid DTO Object sent when looking for ticket");
+        }
     };
 
 };
