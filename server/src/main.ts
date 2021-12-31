0206bb2bf59e5f5fdb2cbd47a0594a62d3389860
Cameron Thacker <cthacker@udel.edu>
2021-12-31 05:37:10 -0500

    Implemented basic swagger module attached to /api endpoint

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { ConnectionOptions, createConnection } from 'typeorm';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const cors = require('cors');
 const development = require('../config/development.json');
 
@@ -21,6 +22,16 @@ async function bootstrap() {
 
     app.enableCors();
     app.use(cors());
+    
+    const config = new DocumentBuilder()
+    .setTitle('Example Ticketing System')
+    .setDescription('The Ticketing API Description')
+    .setVersion('1.0')
+    .addTag('tickets')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(3005);
 }
