9bad3622f6584de4e3dd9f5abd98aae71c456a47
Cameron Thacker <cthacker@udel.edu>
2022-01-03 05:40:14 -0500

    Added successful encryption of user data when adding to database, and clarification of past commit, I did not add guards for adding the user, I plan on adding them for finding the user

--- server/src/modules/user/pipeline/transform/user.add.pipe.ts
+++ server/src/modules/user/pipeline/transform/user.add.pipe.ts
@@ -1,3 +1,4 @@
+import { EncryptionService } from './../../../authentication/encrypt/encrypt.auth.service';
 import { UserEntity } from 'src/shared/entities/mongodb/User.entity';
 import { plainToClass } from 'class-transformer';
 import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
@@ -11,7 +12,7 @@ import { AddUserDTO } from "src/shared/api/private/post/AddUser.dto";
 @Injectable()
 export class UserAddPipe implements PipeTransform {
 
-    constructor (private readonly authService: AuthenticationService, private readonly dataService: DataService) {}
+    constructor (private readonly authService: AuthenticationService, private readonly dataService: DataService, private readonly encryptService: EncryptionService) {}
 
     async transform(value: AddUserDTO, metadata: ArgumentMetadata) {
 
@@ -22,7 +23,12 @@ export class UserAddPipe implements PipeTransform {
             const authUserResult = await this.authService.usernameCount(value.username);
             console.log('result = ', authUserResult);
             if (authUserResult === 0) {
-                return plainToClass(UserEntity, value);
+                const convertedEntity: UserEntity = plainToClass(UserEntity, value);
+                return {
+                    ...convertedEntity,
+                    username: await this.encryptService.encrypt_caesar(convertedEntity.username),
+                    password: await this.encryptService.encrypt_caesar(convertedEntity.password)
+                }
             } else {
                 throw new BadRequestException('Invalid request to add user : User Already Exists');
             }
