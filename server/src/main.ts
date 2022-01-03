e3df5c13e63c92b04dc01d950acf31ad48dfc915
Cameron Thacker <cthacker@udel.edu>
2022-01-03 04:13:47 -0500

    Added entity globbing in mongo configuration in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { ConnectionOptions, createConnection } from 'typeorm';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { TicketEntity } from './shared/entities/mongodb/public/TicketEntity.entity';
+import { UserEntity } from './shared/entities/mongodb/public/User.entity';
 const cors = require('cors');
 const development = require('../config/development.json');
 
@@ -17,20 +19,20 @@ async function bootstrap() {
         name: configService.get('mongo')['name'],
         host: configService.get('mongo')['host'],
         url: configService.get('mongo')['url'],
-        entities: configService.get('mongo')['entities']
+        entities: [TicketEntity, UserEntity, './shared/entities/mongodb/public/*.ts', './shared/entities/mongodb/private/*/*.ts']
     });
 
     app.enableCors();
     app.use(cors());
     
-    const config = new DocumentBuilder()
+    const swaggerConfig = new DocumentBuilder()
     .setTitle('Example Ticketing System')
     .setDescription('The Ticketing API Description')
     .setVersion('1.0')
     .addTag('tickets')
     .build();
 
-    const document = SwaggerModule.createDocument(app, config);
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
     SwaggerModule.setup('api', app, document);
 
     await app.listen(3005);
